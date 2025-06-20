8c6521a580793cb258fe06f89b38611c6960fcbf
HugoCLU <147380234+HugoCLU@users.noreply.github.com>
2025-06-20 16:51:01 +0200

    [UPG] incompatibility logic

--- sale_loyalty_incompatibility/models/sale_order.py
+++ sale_loyalty_incompatibility/models/sale_order.py
@@ -15,24 +15,18 @@ class SaleOrder(models.Model):
         order_programs = self.order_line.reward_id.program_id
 
         promo_programs = programs.filtered(lambda p: p.program_type == "promo_code")
-        print("Promo programs:", promo_programs.mapped("name"))
         incompatible_programs = promo_programs.filtered(
             lambda p: p.is_incompatible_promotion_all == True
         )
+        program_to_remove = None
+        if promo_programs:
+            print(
+                f"Incompatible programs: {incompatible_programs.mapped('name')}"
+            )
+            if len(incompatible_programs) > 0 and len(promo_programs) > 1:
+                program_to_remove = promo_programs[-1]
 
         for program in result:
-            # if promo_programs and program.is_incompatible_promotion_all:
-            #     print(
-            #         f"Incompatible programs: {incompatible_programs.mapped('name')}"
-            #     )
-            #     if len(incompatible_programs) > 0 and len(promo_programs) > 1:
-            #         result[promo_programs[-1]] = {
-            #             "error": _(
-            #                 "This promotion is incompatible with other promotions "
-            #                 "already applied in the order so it can't be applied."
-            #             )
-            #         }
-
             if any({x in order_programs for x in program.incompatible_promotion_ids}):
                 result[program] = {
                     "error": _(
@@ -40,4 +34,13 @@ class SaleOrder(models.Model):
                         "order so it can't be applied."
                     )
                 }
+        if program_to_remove:
+            result[program_to_remove] = {
+                "error": _(
+                    "This promotion is incompatible with other promotions already "
+                    "applied in the order so it can't be applied."
+                )
+            }
+
+        print(result)
         return result
